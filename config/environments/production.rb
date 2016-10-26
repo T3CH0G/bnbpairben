688ee30c3dcd5e64125a87a7f084549e0909f38e
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-10-26 15:41:03 +0800

    for css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,6 @@ Searchkick.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.serve_static_assets = true
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
