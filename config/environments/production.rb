39a812175269ccb931b2c5da8fb4d0bcdf4c04e4
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-10-26 16:33:21 +0800

    css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,17 +24,13 @@ Searchkick.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  config.cache_classes = true
-config.serve_static_files = true
 config.assets.compile = true
-config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
