ea0ab26415013be416662df285f4c5ba25fa7950
Benjamin Phang <BenPhang@Benjamins-MacBook-Pro.local>
2016-10-26 14:56:01 +0800

    push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -76,5 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
 end
